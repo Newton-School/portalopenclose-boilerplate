ee4e0e022888f6dacf09516456c87a7b6a8a85b4
Kush Vasaniya <vasaniyakush@gmail.com>
2023-09-04 12:24:23 +0530

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
 
 const Modal = ({ children, isOpen, onClose }) => {
-  if (!isOpen) return null;
 
-  return ReactDOM.createPortal(
+
+
     <div style={{
       position: 'fixed',
       top: 0,
@@ -15,20 +15,20 @@ const Modal = ({ children, isOpen, onClose }) => {
       justifyContent: 'center',
       alignItems: 'center'
     }}
-    onClick={onClose}
+ 
     >
       <div style={{
         backgroundColor: 'white',
         padding: '2rem',
         position: 'relative'
       }}
-      onClick={(e) => e.stopPropagation()} // This prevents the modal from closing when clicking inside
+  
       >
         {children}
       </div>
     </div>,
     document.body
-  );
+
 };
 
 export default Modal;
